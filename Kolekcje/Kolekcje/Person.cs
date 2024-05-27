afaca566bf26e025b85b20b5a5f997117c921cc8
Mateusz Majewski <mat.maj1992@gmail.com>
2024-05-27 11:34:22 +0200

    LINQ

---
+++ Kolekcje/Kolekcje/Person.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolekcje
+{
+    internal class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        //private DateTime dateOfBirth;
+
+        public DateTime DateOfBirth { get; set; }
+
+        public static int Count = 0;
+
+        private string contactNumber;
+
+        public string ContactNumber
+        {
+            get { return contactNumber; }
+            set 
+            { 
+                if (value.Length < 9 )
+                {
+                    Console.WriteLine("Invalid contact number");
+                } 
+                else
+                {
+                contactNumber = value; 
+                }
+            }
+        }
+
+        //public string ContactNumber {  get; set; }
+
+        //Konstruktor Person
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Count++;
+        }
+
+        //Rozszerzenie konstruktora
+        public Person(DateTime dateOfBirth, string firstName, string lastName) : this(firstName, lastName)
+        {
+            DateOfBirth = dateOfBirth;
+        }
+
+        //public void SetDateOfBirth(DateTime date)
+        //{
+        //    if(date > DateTime.Now)
+        //    {
+        //        Console.WriteLine("Invalid date of birth");
+        //    } 
+        //    else
+        //    {
+        //        dateOfBirth = date;
+        //    }
+        //}
+
+        //public DateTime GetDateOfBirth()
+        //{
+        //    return dateOfBirth;
+        //}
+
+        public void SayHi()
+        {
+            Console.WriteLine($"Hi, I'm {FirstName} {LastName}, {DateOfBirth}");
+        }
+    }
+}
--- Kolekcje/Kolekcje/Program.cs
+++ Kolekcje/Kolekcje/Program.cs
@@ -1,44 +1,119 @@
 ﻿using System.Threading.Tasks;
+using System.Linq;
 
 namespace Kolekcje
 {
     class Program
-    {
+    { 
         public static void Main(string[] args)
         {
 
-            static void DisplayElements(List<int> list)
+            static List<Person> GetEmployees()
             {
-                Console.WriteLine("** List **");
-                foreach (int i in list)
+                List<Person> employees = new List<Person>()
                 {
-                    Console.Write($"{i}, ");
-                }
-
-                Console.WriteLine();
+                    new Person(new DateTime(1990, 2, 2), "Bill", "Wick"),
+                    new Person(new DateTime(1995, 3, 4), "John", "Wick"),
+                    new Person(new DateTime(1996, 4, 2), "Ollie", "Wick"),
+                    new Person(new DateTime(2001, 5, 3), "Dollie", "Wick"),
+                    new Person(new DateTime(2000, 6, 2), "Martha", "Wick"),
+                    new Person(new DateTime(2005, 7, 1), "Bob", "Wick"),
+                    new Person(new DateTime(2003, 7, 1), "Ed", "Wick")
+                };
+                return employees;
             }
 
-            int[] intArray = { 1, 2, 3, 4, 5 };
-            int arrayLenght = intArray.Length;
+            Console.WriteLine("Pracownicy:");
+            List<Person> employees = GetEmployees();
+            List<Person> youngEmployees = new List<Person>();
 
-            List<int> intList = new List<int>() { 6, 1, 20, 3, 45, 60, 100, 2 };
-            DisplayElements(intList);
+            foreach (Person employee in employees)
+            {
+                if(employee.DateOfBirth > new DateTime(2000,1,1))
+                {
+                    youngEmployees.Add(employee);
+                }
+            }
 
-            Console.WriteLine("New Element: ");
-            string userInput = Console.ReadLine();
-            int intValue = int.Parse(userInput);
+            Console.WriteLine("Young employees: " + youngEmployees.Count);
 
-            intList.Add(intValue);
+            Person bob = null;
+            foreach (Person youngEmployee in youngEmployees)
+            {
+                if (youngEmployee.FirstName == "Bob")
+                {
+                    bob = youngEmployee;
+                    break;
+                }
+            }
+            if (bob != null)
+            {
+                bob.SayHi();
+            } else
+            {
+                Console.WriteLine("Bob not found");
+            }
 
-            DisplayElements(intList);
+            Console.WriteLine("LINQ");
 
-            Console.WriteLine("** Remove Range **");
-            intList.RemoveRange(1, 2);
-            DisplayElements(intList);
+            bool EmployeeIsYoung(Person employee)
+            {
+                return employee.DateOfBirth > new DateTime(2000, 1, 1);
+            }
+            List<Person> youngEmployeesLINQ = employees.Where(EmployeeIsYoung).ToList();
 
-            Console.WriteLine("Sort");
-            intList.Sort();
-            DisplayElements(intList);
+            bool EmployeeIsEd(Person employee)
+            {
+                return employee.FirstName == "Ed";
+            }
+            Person edLINQ = youngEmployees.FirstOrDefault(EmployeeIsEd);
+
+            if (edLINQ != null)
+            {
+                edLINQ.SayHi();
+            }
+            else
+            {
+                Console.WriteLine("Ed not found");
+            }
+
+
+
+
+            Console.WriteLine("************");
+
+            //static void DisplayElements(List<int> list)
+            //{
+            //    Console.WriteLine("** List **");
+            //    foreach (int i in list)
+            //    {
+            //        Console.Write($"{i}, ");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+
+            //int[] intArray = { 1, 2, 3, 4, 5 };
+            //int arrayLenght = intArray.Length;
+
+            //List<int> intList = new List<int>() { 6, 1, 20, 3, 45, 60, 100, 2 };
+            //DisplayElements(intList);
+
+            //Console.WriteLine("New Element: ");
+            //string userInput = Console.ReadLine();
+            //int intValue = int.Parse(userInput);
+
+            //intList.Add(intValue);
+
+            //DisplayElements(intList);
+
+            //Console.WriteLine("** Remove Range **");
+            //intList.RemoveRange(1, 2);
+            //DisplayElements(intList);
+
+            //Console.WriteLine("Sort");
+            //intList.Sort();
+            //DisplayElements(intList);
             
         }
     }
