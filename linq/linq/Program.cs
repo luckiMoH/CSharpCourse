95aeb83f39e22fdb2631451c95f5783c47ea278b
Mateusz Majewski <mat.maj1992@gmail.com>
2024-06-05 16:04:17 +0200

    Projekcja danych. Select

--- linq/linq/Program.cs
+++ linq/linq/Program.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using CsvHelper;
 using CsvHelper.Configuration;
+using linq;
 
 // ReSharper disable UseFormatSpecifierInInterpolation
 
@@ -20,14 +21,49 @@ namespace FirstProject
             var googleApps = LoadGoogleAps(csvPath);
 
             //Display(googleApps);
-            GetData(googleApps);
+            //GetData(googleApps);
+            ProjectData(googleApps);
 
+        }
+
+        static void ProjectData(IEnumerable<GoogleApp> googleApps)
+        {
+            var highRatedBeautyApps = googleApps.Where(app => app.Rating > 4.6m && app.Category == Category.BEAUTY);
+            var highRatedBeautyAppsNames = highRatedBeautyApps.Select(app => app.Name).ToList();
+
+            var dtos = highRatedBeautyApps.Select(app => new GoogleAppDto()
+            {
+                Reviews = app.Reviews,
+                Name = app.Name
+            });
+
+            var anonymousDtos = highRatedBeautyApps.Select(app => new
+            {
+                Reviews = app.Reviews,
+                Name = app.Name,
+                Category = app.Category
+            });
+
+            foreach (var dto in anonymousDtos)
+            {
+                Console.WriteLine(dto.Name + ": " + dto.Reviews);
+            }
+
+            //foreach (var dto in dtos)
+            //{
+            //    Console.WriteLine(dto.Name + ": " + dto.Reviews );
+            //}
+
+            //var genres = highRatedBeautyApps.SelectMany(app => app.Genres);
+            //Console.WriteLine(string.Join(";", genres));
+
+            //Console.WriteLine(string.Join(", ", highRatedBeautyAppsNames));
         }
 
         static void GetData(IEnumerable<GoogleApp> googleApps)
         {
             var highRatedApps = googleApps.Where( app => app.Rating > 4.6m);
-            var highRatedBeautyApps = highRatedApps.Where(app => app.Rating >4.6m && app.Category == Category.BEAUTY );
+            var highRatedBeautyApps = googleApps.Where(app => app.Rating >4.6m && app.Category == Category.BEAUTY );
             Display(highRatedBeautyApps);
 
             var firstHighRatedBeautyApp = highRatedBeautyApps.SingleOrDefault(app => app.Reviews < 30);
