080f7a501abfc57409d808a0154c447a2c6491bd
Mateusz Majewski <mat.maj1992@gmail.com>
2024-06-06 10:54:18 +0200

    dzielenie danych. Take, TakeLast, TakeWhile, Skip, SkipWhile

--- linq/linq/Program.cs
+++ linq/linq/Program.cs
@@ -22,8 +22,32 @@ namespace FirstProject
 
             //Display(googleApps);
             //GetData(googleApps);
-            ProjectData(googleApps);
+            //ProjectData(googleApps);
+            DivideData(googleApps);
 
+        }
+
+        static void DivideData(IEnumerable<GoogleApp> googleApps)
+        {
+            var highRatedBeautyApps = googleApps.Where(app => app.Rating > 4.4m && app.Category == Category.BEAUTY);
+
+            var first5HighRatedApps = highRatedBeautyApps.Take(5);
+            var last5HighRatedApps = highRatedBeautyApps.TakeLast(5);
+            var appsWithReviesWhile1000 = highRatedBeautyApps.TakeWhile(app => app.Reviews > 1000);
+            var skippedResults = highRatedBeautyApps.Skip(5);
+            var skippedResultsWhileReviewsOver1000 = highRatedBeautyApps.SkipWhile(app => app.Reviews > 1000);
+
+            Display(highRatedBeautyApps);
+            Console.WriteLine("*****");
+            Display(first5HighRatedApps);
+            Console.WriteLine("*****");
+            Display(last5HighRatedApps);
+            Console.WriteLine("*****");
+            Display(appsWithReviesWhile1000);
+            Console.WriteLine("*****");
+            Display(skippedResults);
+            Console.WriteLine("*****");
+            Display(skippedResultsWhileReviewsOver1000);
         }
 
         static void ProjectData(IEnumerable<GoogleApp> googleApps)
