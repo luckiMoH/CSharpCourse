ebd8946f8c565844d11289842d150ff2617f5314
Mateusz Majewski <mat.maj1992@gmail.com>
2024-06-18 10:03:47 +0200

    programowanie asynchroniczne

---
+++ ProgramowanieAsynchroniczne/ProgramowanieAsynchroniczne/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProgramowanieAsynchroniczne
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
